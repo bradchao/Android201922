4b57cff23bd04391ead05b8e93ef78d1c66d737b
brad <brad@brad.tw>
2019-12-25 20:40:50 +0800

    3

--- app/src/main/java/tw/org/iii/android201922/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201922/MainActivity.java
@@ -8,7 +8,11 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,10 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
     private void init(){
+        myBTReceiver = new MyBTReceiver();
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 1);
+        }else{
+            discoverBTDevice();
         }
     }
 
@@ -52,6 +59,29 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK){
             Log.v("brad", "BT OK");
+            discoverBTDevice();
         }
     }
+
+    private void discoverBTDevice(){
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myBTReceiver, filter);
+
+        bluetoothAdapter.startDiscovery();
+
+    }
+
+    private MyBTReceiver myBTReceiver;
+
+    private class MyBTReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            String deviceName = device.getName();
+            String deviceMAC = device.getAddress(); // MAC address
+
+            Log.v("brad", deviceName + ":" + deviceMAC);
+        }
+    }
+
 }
