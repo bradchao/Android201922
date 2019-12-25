6540259313a6c3bec7f33bc7c9898095ffd88ee9
brad <brad@brad.tw>
2019-12-25 20:28:36 +0800

    2

--- app/src/main/java/tw/org/iii/android201922/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201922/MainActivity.java
@@ -1,13 +1,17 @@
 package tw.org.iii.android201922;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +38,20 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    private BluetoothAdapter bluetoothAdapter;
     private void init(){
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Log.v("brad", "BT OK");
+        }
     }
 }
